5c1878749e3d789f67259136ff2c67b8c3922e93
GABRIEL GUIMARAES DE OLIVEIRA 114111358 <gabriel.oliveira@ccc.ufcg.edu.br>
2018-09-05 15:42:22 -0300

    adding crud routes with some bodies

--- routes/user.router.js
+++ routes/user.router.js
@@ -4,7 +4,35 @@ const router = express.Router();
 
 router.get('/', (_, res) => {    
     res.status(200).json({
-        data: 'GET User works!'
+        data: "GET User works!"
+    })
+});
+
+router.post('/', (req, res) => {    
+    res.status(200).json({
+        data: req.body
+    })
+});
+
+
+router.put('/:id', (req, res) => {    
+    res.status(200).json({
+        id: req.params.id,
+        data: req.body
+    })
+});
+
+
+router.delete('/', (_, res) => {    
+    res.status(200).json({
+        data: "You deleted all users, so i'm fully empty hauehaue"
+    })
+});
+
+router.delete('/:id', (req, res) => {    
+    res.status(200).json({
+        id: req.params.id,
+        data: `You delete this user with id ${req.params.id}, so i don't exist anymore ahuehaueh`
     })
 });
 
